662839e6a6f168726d1da49c44c86a6d20d46c44
Mathias Rendon <mathiassaid7@gmail.com>
2025-02-03 13:42:25 -0500

    Add header in default error and change some colors

--- src/error/DefaultError.jsx
+++ src/error/DefaultError.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
-import dogReading from '@public/dogReading.gif'
+import dogReading from '@public/images/dogReading.gif'
 import '@styles/Error.css'
+import Header from "@components/Header";
 
 function DefaultError() {
     return (
         <>
+        <Header/>
         <section className="error-section">
             <img
             src={dogReading}
@@ -12,7 +14,7 @@ function DefaultError() {
             />
             <h2>Oh no! Something is bad D:</h2>
             <p>This site is in maintanance or doesn&apos;t exist. Please <Link to="/" className="link">click here</Link> to go back</p>
-            <p className="quote">Also here&apos;s a dog reading! :D</p>
+            <p className="quote">Btw here&apos;s a dog reading! :D</p>
         </section>
         </>
     )
