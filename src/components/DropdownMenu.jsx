2a45d7918ad202282d2898d16f3d8b4696f80a00
Mathias Rendon <mathiassaid7@gmail.com>
2025-02-04 14:58:12 -0500

    Feat: Add dropdown menu with hover animation

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -7,7 +7,6 @@ import upArrow from '@public/dropdownUp.svg'
 function DropDownMenu() {
     const [isOpen, setIsOpen] = useState(false)
 
-
     const handleMouseEnter = () => {
         setIsOpen(true)
     }
@@ -16,18 +15,17 @@ function DropDownMenu() {
         setIsOpen(false)
     }
 
-
     return (
         <>
         <div className="dropdown-menu" onMouseEnter={() => handleMouseEnter()} onMouseLeave={() => handleMouseLeave()}>
             <button className="dropbtn">
-                Menu {isOpen ? <img src={upArrow} alt="close dropdown menu"/> : <img src={downArrow} alt="Open dropdown menu"/>}
+                Información {isOpen ? <img src={upArrow} alt="close dropdown menu"/> : <img src={downArrow} alt="Open dropdown menu"/>}
             </button>
             {isOpen && (
                 <div className="dropdown-content" onMouseOver={() => setIsOpen(true)}>
-                    <Link to="/d" className="dropdown-link">One</Link>
-                    <Link to="/" className="dropdown-link">Two</Link>
-                    <Link to="/" className="dropdown-link">Three</Link>
+                    <Link to="/d" className="dropdown-link">BookHive</Link>
+                    <Link to="/" className="dropdown-link">Creador</Link>
+                    <Link to="/" className="dropdown-link">Contacto</Link>
                 </div>
             )}
         </div>
