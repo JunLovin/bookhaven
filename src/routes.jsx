97c90dc9a969376909cbabf2f41ed4671c8cec55
Mathias Rendon <mathiassaid7@gmail.com>
2025-02-04 14:59:30 -0500

    Feat: add routes and dynamic routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,16 @@
 import App from './App'
 import DefaultError from './error/DefaultError'
+import BookDetail from '@components/BookDetail';
 
 const routes = [
     {
         path: '/',
         element: <App/>,
+        errorElement: <DefaultError/>,
+    },
+    {
+        path: '/book/:id',
+        element:  <BookDetail/>,
         errorElement: <DefaultError/>
     }
 ]
