2fb9b0e91cf4c56fffef133040d267ec6ba7b9ac
Mathias Rendon <mathiassaid7@gmail.com>
2025-02-04 20:42:58 -0500

    Change relative import

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Body from "./components/Body"
-import Header from "./components/Header"
+import Header from "@components/Header"
 
 function App() {
   return (
     <>
     <Header/>
-    <Body/>
     </>
   )
 }
