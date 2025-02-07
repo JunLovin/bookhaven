a8ea29c56bed66e2a411454f44e12e4192e29f84
Mathias Rendon <mathiassaid7@gmail.com>
2025-02-07 12:30:11 -0500

    Add link to my github profile

--- src/components/BookHavenInfo.jsx
+++ src/components/BookHavenInfo.jsx
@@ -9,7 +9,7 @@ function BookHavenInfo() {
                 <h2>Welcome to BookHaven!</h2>
                 <p>This page is didactic, all books are from <a href="https://books.google.com" target="_blank">Google Books</a>, I used their API to make this page reality.</p>
                 <br/>
-                <p>If you have feedback please contact me through my GitHub, i will be greatful!</p>
+                <p>If you have feedback please contact me through my <a href="https://github.com/JunLovin" target='_blank'>GitHub</a>, i will be greatful!</p>
                 <Link to="/" className="goback">Click here to go back</Link>
                 <p className="btw">Btw, It&apos;s my first web with functionality and it cost me so much! And I know, this isn&apos; the best web of all time. I appreciate if you have feedback and let me know your ideas.</p>
             </div>
