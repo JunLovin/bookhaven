231dbccd507fb649762a14125094d773abc53de7
Mathias Rendon <mathiassaid7@gmail.com>
2025-02-07 12:29:38 -0500

    Add recommended section below the book info

--- src/components/BookDetail.jsx
+++ src/components/BookDetail.jsx
@@ -9,11 +9,17 @@ import heart from '@public/heart.svg'
 import searchIcon from '@public/search.svg'
 import shoppingBag from '@public/shoppingBag.svg'
 import Card from './Card'
+import Footer from './Footer'
+
+const bestSeller = ['VpQnEQAAQBAJ', 'Qu_GEAAAQBAJ', 'TSQUEQAAQBAJ', '89b5EAAAQBAJ', 'sbybDwAAQBAJ', 'OJjkEAAAQBAJ', 'fWxdDwAAQBAJ', 'AW24EAAAQBAJ', 'ryVnDwAAQBAJ', '1jwDEAAAQBAJ', 'reSgAAAAQBAJ', 'Rr86EQAAQBAJ']
+
 
 function BookDetail() {
     const { id } = useParams()
     const [book, setBook] = useState(null)
     const [error, setError] = useState(null)
+
+    const [bestSellerData, setBestSellerData] = useState([])
 
     const [search, setSearch] = useState('')
     const [data, setData] = useState([])
@@ -39,7 +45,6 @@ function BookDetail() {
     }
     console.log(data)   
 
-
     useEffect(() => {
         fetch(`https://www.googleapis.com/books/v1/volumes/${id}`)
         .then((res) => res.json())
@@ -47,15 +52,34 @@ function BookDetail() {
         .catch((err) => setError(err))
     }, [id])
 
+    useEffect(() => {
+        const fetchBestSellers = async () => {
+            try {
+                const promises = bestSeller.map(id => 
+                    fetch(`https://www.googleapis.com/books/v1/volumes/${id}`)
+                    .then(res => res.json())
+                )
+                const results = await Promise.all(promises)
+                setBestSellerData(results)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        fetchBestSellers()
+    }, [])
+
     if (error || errorSearch) {
         return <div>Error: {error.message}</div>
     }
 
-
-
     const handleClick = url => {
         window.open(url, '_blank')
     }
+
+    const handleBuy = () => {
+        alert('Added to shopping bag!');
+    }
+
     if (!book) {
         <div>Loading...</div>
     } else {
@@ -66,7 +90,7 @@ function BookDetail() {
                 <DropDownMenu className="dropdown-mobile" id="dropdown-mobile" firstLi="One" secondLi="Two" thirdLi="Three" menuTitle="Opciones"/>
                 <div className="links-list">
                 <ul>
-                    <li><Link to="/d" className="link">One</Link></li>
+                    <li><Link to="/BookHaven" className="link">One</Link></li>
                     <li><Link to="/d" className="link">Two</Link></li>
                     <li><Link to="/d" className="link">Three</Link></li>
                     <DropDownMenu className="dropdown-menu" firstLi="BookHaven" secondLi="Creator" thirdLi="Contact" menuTitle="Información"/>
@@ -97,34 +121,56 @@ function BookDetail() {
         <div className="cards-container">
             {isShow && data.map((element, index) => {
                 return (
-                <Link to={`/book/${element.id}`} key={index} className="link">
-                    <Card items={element} id={element.id} onClick={() => setIsShow(false)}/>
-                </Link>
+                <>
+                    <Link to={`/book/${element.id}`} key={index} className="link">
+                        <Card items={element} id={element.id} onClick={() => setIsShow(false)}/>
+                    </Link>
+                </>
             )
             })}
+            {isShow && <Footer isShow={isShow}/>}
         </div>
         {!isShow && 
             <section className="bookDetailSection">
                 <div className="book-container">
                     <div className="book-image-preview">
-                        <img
-                        src={book.volumeInfo.imageLinks.large ? book.volumeInfo.imageLinks.large : book.volumeInfo.imageLinks.thumbnail}
-                        alt={book.volumeInfo.title}
-                        />
+                        {book && book.volumeInfo && book.volumeInfo.imageLinks ? (
+                            <img
+                                src={book.volumeInfo.imageLinks.large ? book.volumeInfo.imageLinks.large : book.volumeInfo.imageLinks.thumbnail}
+                                alt={book.volumeInfo.title}
+                            />
+                        ) : (
+                            <div>Image not available</div>
+                        )}
                     </div>
                     <div className="book-info">
-                        <h1>{book.volumeInfo.title}</h1>
-                        <h2>Author: {book.volumeInfo.authors ? book.volumeInfo.authors.map((element, index) => {return (<span key={index}>{element}</span>)}) : <span>Not Founded</span>}</h2>
-                        <p>{book.volumeInfo.description ? book.volumeInfo.description : 'Description not founded'}</p>
+                        <h1>{book && book.volumeInfo ? book.volumeInfo.title : 'Title not available'}</h1>
+                        <h2>Author: {book && book.volumeInfo && book.volumeInfo.authors ? book.volumeInfo.authors.map((element, index) => (
+                            <span key={index}>{element}</span>
+                        )) : <span>Not Found</span>}</h2>
+                        <p>{book && book.volumeInfo && book.volumeInfo.description ? book.volumeInfo.description : 'Description not found'}</p>
                         <div className="buy-book">
-                            <p>Price: {book.saleInfo.listPrice ? <span>{book.saleInfo.listPrice.amount}</span> : <span>Not found</span>}{book.saleInfo.listPrice ? <span>{book.saleInfo.listPrice.currencyCode}</span> : <span></span>}</p>
+                            <p>Price: {book && book.saleInfo && book.saleInfo.listPrice ? <b>{book.saleInfo.listPrice.amount}</b> : <b>Not found</b>}{book && book.saleInfo && book.saleInfo.listPrice ? <b> {book.saleInfo.listPrice.currencyCode}</b> : <b></b>}</p>
                             <div className="buy-book-btns">
-                                <button className="buy-btn">Buy</button>
-                                <button className="preview-btn" onClick={() => handleClick(book.volumeInfo.previewLink)}>See preview</button>
+                                <button className="buy-btn" onClick={() => handleBuy()}>Buy</button>
+                                {book && book.volumeInfo && book.volumeInfo.previewLink && (
+                                    <button className="preview-btn" onClick={() => handleClick(book.volumeInfo.previewLink)}>See preview</button>
+                                )}
                             </div>
                         </div>
                     </div>
                 </div>
+                <section className="books-recommended">
+                    <div className="books-recommended-title">
+                        <h2>Libros recomendados</h2>
+                    </div>
+                    <div className="books">
+                    {bestSellerData.length > 0 ? bestSellerData.map((book, index) => (
+                            <Link key={index} className="books-recomendados" to={`/book/${book.id}`}><Card items={book} className="book-card" /></Link>
+                        )) : <div>Loading...</div>}
+                    </div>
+                </section>
+                <Footer isShow={!isShow}/>
             </section>
     }
             </>
