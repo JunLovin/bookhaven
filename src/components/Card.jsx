a80786a4297723d227791d0cec357a709ee3b0c2
Mathias Rendon <mathiassaid7@gmail.com>
2025-02-07 12:31:36 -0500

    Add some props and make classNameImage customizable for all sections

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,13 @@
-import { Link } from 'react-router-dom';
 import '@styles/Card.css'
 
-function Card({ items, onClick, className = 'card-container' }) {
-    const volumeInfo = items.volumeInfo || {}; // Default to an empty object if undefined
+function Card({ items, onClick, className = 'card-container', description = true, author = true, classNameImage = 'card-image-container' }) {
+    const volumeInfo = items.volumeInfo || {};
 
     return (
         <>
         <div className={className} onClick={onClick}>
             <div className="card">
-                <div className="card-image-container">
+                <div className={classNameImage}>
                     {volumeInfo.imageLinks && volumeInfo.imageLinks.thumbnail ? (
                         <img
                         src={volumeInfo.imageLinks.thumbnail}
@@ -21,8 +20,15 @@ function Card({ items, onClick, className = 'card-container' }) {
                 </div>
                 <div className="card-info-container">
                     <h2>{volumeInfo.title || 'Title Not Available'}</h2>
-                    <h3>Author(s): {volumeInfo.authors ? volumeInfo.authors.map((element, index) => <span key={index}>{element}</span>) : <span>Not Available</span>}</h3>
-                    <p>{volumeInfo.description && volumeInfo.description.length < 1500 ? volumeInfo.description : <span>Description Not Available</span>}</p>
+                    <h3>
+                        {author && (
+                            <>
+                                <span>Author(s):</span> 
+                                {volumeInfo.authors ? volumeInfo.authors.map((element, index) => <span key={index}>{element}</span>) : <span>Not Available</span>}
+                            </>
+                        )}
+                    </h3>
+                    <p>{description && (volumeInfo.description && volumeInfo.description.length < 1500 ? volumeInfo.description : <span>Description Not Available</span>)}</p>
                 </div>
             </div>
         </div>
